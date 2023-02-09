3318ce2fe3c0593ea111f4746647c147ffb723df
Link <stereo@mail.com>
2023-02-09 17:35:57 +0300

    Work_5

--- Work_Lesson5/task_2.java
+++ Work_Lesson5/task_2.java
@@ -1,49 +1,55 @@
 package Work_Lesson5;
-//Пусть дан список сотрудников: Иван, Пётр, Антон и так далее.
-//Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
 import java.util.*;
-import java.util.HashMap;
-import java.util.Map;
+
 public class task_2 {
-public static void main(String[] args) {
-    // String nameWorkers = "Кристина Белова"+
-    // "Анна Мусина"+
-    // "Анна Крутова"+    
-    // "Иван Юрин"+
-    // "Петр Лыков"+
-    // "Павел Чернов"+
-    // "Петр Чернышов"+
-    // "Мария Федорова"+
-    // "Марина Светлова"+
-    // "Иван Савин"+
-    // "Мария Рыкова"+
-    // "Марина Лугова"+
-    // "Анна Владимирова"+
-    // "Иван Мечников"+
-    // "Петр Петин"+
-    // "Иван Ежов";
-    // Map<String, Integer> occurrences = new HashMap<String, Integer>();
-
-    Map<String, String> map = new HashMap<String, String>();
-    map.put("Мусина","Анна");
-    map.put("Крутова","Анна");
-    map.put("Юрин","Иван");
-    map.put("Лыков","Петр");
-    map.put("Лыков","Петр");
-    map.put("Лыков","Петр");
-    map.put("Лыков","Петр");
-    map.put("Лыков","Петр");
-    map.put("Лыков","Петр");
+    public static void main(String[] args) {
+        Map<String, Integer> name = new HashMap<>();
+        String strPeople =
+                "Светлана Петрова " +
+                "Кристина Белова " +
+                "Анна Мусина " +
+                "Анна Крутова " +
+                "Иван Юрин " +
+                "Петр Лыков " +
+                "Павел Чернов " +
+                "Петр Чернышов " +
+                "Мария Федорова " +
+                "Марина Светлова " +
+                "Мария Савина " +
+                "Мария Рыкова " +
+                "Марина Лугова " +
+                "Анна Владимирова " +
+                "Иван Мечников " +
+                "Петр Петин " +
+                "Иван Ежов ";
+        String[] list = strPeople.split(" ");
+        for (int i = 0; i < list.length; i += 2) {
+            if (name.containsKey(list[i])) {
     
-    Set<Object> uniqueValues = new HashSet<Object>(map.values());
-    for (int j = 0; j <  map.size(); j++) {
-        
-        if(map.get(key) )
-        
-    }
+                name.replace(list[i], name.get(list[i]) + 1);
+            } else {
+                name.put(list[i], 1);
+            }
+        }
 
-    int newValue =map.getOrDefault(i, 0) + 1;
-    System.out.println(uniqueValues);
+        Map<String, Integer> sortedName = new LinkedHashMap<>();
+        int max = 1;
+        for (int value : name.values()) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        for (int i = max; i > 0; i--) {
+            for (Map.Entry<String, Integer> entry : name.entrySet()) {
+                String key = entry.getKey();
+                if (name.get(key) == i) {
+                    sortedName.put(key, name.get(key));
+
+                }
+                
+            }
+        }
+        System.out.print(sortedName);
     }
     
 }
